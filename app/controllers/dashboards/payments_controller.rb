99ba04d83af3ccd9d9aad3d5fa7890a58930e932
David Alvarado <davidealva@gmail.com>
2015-01-05 15:48:25 -0700

    Slide 8 fixes

--- app/controllers/dashboards/payments_controller.rb
+++ app/controllers/dashboards/payments_controller.rb
@@ -1,8 +1,88 @@
 class Dashboards::PaymentsController < ApplicationController
-layout 'user_dashboard'
+	before_action :set_user, only: [:show, :update]
+	layout 'user_dashboard'
 
 	def index
 		@user = current_user
 	end
 
+	def update
+		if user_params[:certificate].present? 
+      if @user.certificate
+        @user.certificate.update(uploaded_file: user_params[:certificate])
+      else
+        certificate = Certificate.new
+        certificate.uploaded_file = user_params[:certificate]
+        @user.certificate = certificate
+      end
+      
+      user_params.delete(:certificate)
+    end
+
+    if user_params[:certificate_delete].present?
+      current_user.certificate.destroy
+      user_params.delete(:certificate_delete)
+    end
+
+    respond_to do |format|
+      if @user.update(user_params)      
+       
+        if !@user.armor_profile.present?
+          data = ArmorService.new
+          account = data.create_account({
+            'user_name' => current_user.name,
+            'user_email' => current_user.email,
+            'user_phone' => current_user.phone
+          })
+          if account.present?
+            profile = ArmorProfile.create(user_id: current_user.id, armor_account: account.to_i)
+            profile.update(armor_user: data.get_user(account).to_i)
+          end
+        else
+          data = ArmorService.new
+          data.update_user({
+            'account' => current_user.armor_profile.armor_account,
+            'user' => current_user.armor_profile.armor_user,
+            'user_name' => current_user.name,
+            'user_phone' => current_user.phone
+          })
+          data.update_company({
+            'account' => current_user.armor_profile.armor_account,            
+            'company' => current_user.company
+          })
+          data.update_address({
+            'account' => current_user.armor_profile.armor_account,            
+            'address' => current_user.location,
+            'city' => current_user.city,
+            'state' => current_user.state,
+            'postal_code' => current_user.zip_code,
+            'country' => current_user.country
+          })
+          data.create_armor_bank_account({
+            'account' => current_user.armor_profile.armor_account,            
+            'type' => current_user.armor_bank_account.account_type,
+            'location' => current_user.armor_bank_account.account_location,
+            'bank' => current_user.armor_bank_account.account_bank,
+            'routing' => current_user.armor_bank_account.account_routing,
+            'swift' => current_user.armor_bank_account.account_swift,
+            'account' => current_user.armor_bank_account.account_account,
+            'iban' => current_user.armor_bank_account.account_iban
+          })
+        end
+        format.html { redirect_to dashboards_payments_path }
+      else
+        format.html { render action: 'show' }
+    	end
+    end
+	end
+
+	private
+	  def set_user
+	    @user = current_user
+	  end
+
+	  def user_params
+	    params.require(:user).permit!
+	    # (:email, :name, :phone, :company, :location, :facebook, :ein_tax, :asset = [])
+	  end
 end
