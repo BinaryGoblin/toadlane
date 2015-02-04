b9882be2747bc37961cc1e0475fb8323aaeddbdf
Ranjit <rndsdeveloper@gmail.com>
2015-02-05 02:03:27 +0530

    symbol changed

--- app/controllers/dashboards/profiles_controller.rb
+++ app/controllers/dashboards/profiles_controller.rb
@@ -55,17 +55,17 @@ class Dashboards::ProfilesController < ::DashboardsController
         else
           data = ArmorService.new
           data.update_user({
-            'account' => current_user.armor_profile.armor_account,
+            :account => current_user.armor_profile.armor_account,
             'user' => current_user.armor_profile.armor_user,
             'user_name' => current_user.name,
             'user_phone' => current_user.phone
           })
           data.update_company({
-            'account' => "current_user.armor_profile.armor_account",            
+            :account => "current_user.armor_profile.armor_account",            
             'company' => "current_user.company"
           })
           data.update_address({
-            'account' => current_user.armor_profile.armor_account,            
+            :account => current_user.armor_profile.armor_account,            
             'address' => current_user.location,
             'city' => current_user.city,
             'state' => current_user.state,
@@ -73,7 +73,7 @@ class Dashboards::ProfilesController < ::DashboardsController
             'country' => current_user.country
           })
           data.create_armor_bank_account({
-            'account' => current_user.armor_profile.armor_account,            
+            :account => current_user.armor_profile.armor_account,            
             'type' => current_user.armor_bank_account.account_type,
             'location' => current_user.armor_bank_account.account_location,
             'bank' => current_user.armor_bank_account.account_bank,
