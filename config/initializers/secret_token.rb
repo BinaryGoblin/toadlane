4f709fe5c63a4c867af66b812fba58e43fa0d372
Brian Kung <brian@callmekung.com>
2015-03-20 09:15:37 -0500

    Updates secret token for security purposes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Toad::Application.config.secret_key_base = '9bb4f306f5ec0ae1de03162e1c470afe05ace6194b6c1a690515e49741648e3dd7e0f30b6b7d1112926e2c37cbf56f892b7d121d8422b7344ce00947c5ded3c3'
+Toad::Application.config.secret_key_base = '863e21cd3ab2064ad96c136da5f7aff5259d2cea83efce0ed3be09ae5769b7942b211e88511d1bd6ff8cd3321bd9dee260f7aaa7396afec58fd1eb56236864ae'
