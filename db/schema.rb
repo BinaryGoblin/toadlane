f5bbf2f6170610c815ef352b5f416da7c5c1927e
Brian Kung <brian@callmekung.com>
2015-03-26 04:23:59 -0500

    Changes Order IDs to bigint to prevent Postgresql Out of Range bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002211243) do
+ActiveRecord::Schema.define(version: 20150325165059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "armor_bank_accounts", force: true do |t|
+  create_table "armor_bank_accounts", force: :cascade do |t|
     t.integer  "account_type"
-    t.string   "account_location"
-    t.string   "account_bank"
-    t.string   "account_routing"
-    t.string   "account_swift"
-    t.string   "account_account"
-    t.string   "account_iban"
+    t.string   "account_location", limit: 255
+    t.string   "account_bank",     limit: 255
+    t.string   "account_routing",  limit: 255
+    t.string   "account_swift",    limit: 255
+    t.string   "account_account",  limit: 255
+    t.string   "account_iban",     limit: 255
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20141002211243) do
 
   add_index "armor_bank_accounts", ["user_id"], name: "index_armor_bank_accounts_on_user_id", using: :btree
 
-  create_table "armor_invoices", force: true do |t|
+  create_table "armor_invoices", force: :cascade do |t|
     t.integer  "buyer_id"
     t.integer  "seller_id"
     t.integer  "product_id"
@@ -49,12 +49,12 @@ ActiveRecord::Schema.define(version: 20141002211243) do
 
   add_index "armor_invoices", ["product_id"], name: "index_armor_invoices_on_product_id", using: :btree
 
-  create_table "armor_orders", force: true do |t|
+  create_table "armor_orders", force: :cascade do |t|
     t.integer  "buyer_id"
     t.integer  "seller_id"
     t.integer  "account_id"
     t.integer  "product_id"
-    t.integer  "order_id"
+    t.integer  "order_id",           limit: 8
     t.integer  "status"
     t.float    "unit_price"
     t.integer  "count"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20141002211243) do
     t.integer  "invoice_num"
     t.integer  "purchase_order_num"
     t.datetime "status_change"
-    t.string   "uri"
+    t.string   "uri",                limit: 255
     t.boolean  "deleted",                        default: false
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 20141002211243) do
     t.integer  "rebate_percent",                 default: 0
   end
 
-  create_table "armor_profiles", force: true do |t|
+  create_table "armor_profiles", force: :cascade do |t|
     t.integer  "armor_account"
     t.integer  "armor_user"
     t.integer  "user_id"
@@ -83,16 +83,16 @@ ActiveRecord::Schema.define(version: 20141002211243) do
 
   add_index "armor_profiles", ["user_id"], name: "index_armor_profiles_on_user_id", using: :btree
 
-  create_table "categories", force: true do |t|
-    t.string   "name"
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.integer  "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "certificates", force: true do |t|
-    t.string   "filename"
-    t.string   "content_type"
+  create_table "certificates", force: :cascade do |t|
+    t.string   "filename",     limit: 255
+    t.string   "content_type", limit: 255
     t.binary   "data"
     t.integer  "user_id"
     t.datetime "created_at"
@@ -101,47 +101,47 @@ ActiveRecord::Schema.define(version: 20141002211243) do
 
   add_index "certificates", ["user_id"], name: "index_certificates_on_user_id", using: :btree
 
-  create_table "images", force: true do |t|
+  create_table "images", force: :cascade do |t|
     t.integer  "product_id"
-    t.string   "image_file_name"
-    t.string   "image_file_size"
-    t.string   "image_content_type"
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_file_size",    limit: 255
+    t.string   "image_content_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "images", ["product_id"], name: "index_images_on_product_id", using: :btree
 
-  create_table "mailboxer_conversation_opt_outs", force: true do |t|
+  create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.integer "unsubscriber_id"
-    t.string  "unsubscriber_type"
+    t.string  "unsubscriber_type", limit: 255
     t.integer "conversation_id"
   end
 
   add_index "mailboxer_conversation_opt_outs", ["conversation_id"], name: "index_mailboxer_conversation_opt_outs_on_conversation_id", using: :btree
   add_index "mailboxer_conversation_opt_outs", ["unsubscriber_id", "unsubscriber_type"], name: "index_mailboxer_conversation_opt_outs_on_unsubscriber_id_type", using: :btree
 
-  create_table "mailboxer_conversations", force: true do |t|
-    t.string   "subject",    default: ""
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+  create_table "mailboxer_conversations", force: :cascade do |t|
+    t.string   "subject",    limit: 255, default: ""
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  create_table "mailboxer_notifications", force: true do |t|
-    t.string   "type"
+  create_table "mailboxer_notifications", force: :cascade do |t|
+    t.string   "type",                 limit: 255
     t.text     "body"
-    t.string   "subject",              default: ""
+    t.string   "subject",              limit: 255, default: ""
     t.integer  "sender_id"
-    t.string   "sender_type"
+    t.string   "sender_type",          limit: 255
     t.integer  "conversation_id"
-    t.boolean  "draft",                default: false
-    t.string   "notification_code"
+    t.boolean  "draft",                            default: false
+    t.string   "notification_code",    limit: 255
     t.integer  "notified_object_id"
-    t.string   "notified_object_type"
-    t.string   "attachment"
-    t.datetime "updated_at",                           null: false
-    t.datetime "created_at",                           null: false
-    t.boolean  "global",               default: false
+    t.string   "notified_object_type", limit: 255
+    t.string   "attachment",           limit: 255
+    t.datetime "updated_at",                                       null: false
+    t.datetime "created_at",                                       null: false
+    t.boolean  "global",                           default: false
     t.datetime "expires"
   end
 
@@ -150,22 +150,22 @@ ActiveRecord::Schema.define(version: 20141002211243) do
   add_index "mailboxer_notifications", ["sender_id", "sender_type"], name: "index_mailboxer_notifications_on_sender_id_and_sender_type", using: :btree
   add_index "mailboxer_notifications", ["type"], name: "index_mailboxer_notifications_on_type", using: :btree
 
-  create_table "mailboxer_receipts", force: true do |t|
+  create_table "mailboxer_receipts", force: :cascade do |t|
     t.integer  "receiver_id"
-    t.string   "receiver_type"
-    t.integer  "notification_id",                            null: false
-    t.boolean  "is_read",                    default: false
-    t.boolean  "trashed",                    default: false
-    t.boolean  "deleted",                    default: false
+    t.string   "receiver_type",   limit: 255
+    t.integer  "notification_id",                             null: false
+    t.boolean  "is_read",                     default: false
+    t.boolean  "trashed",                     default: false
+    t.boolean  "deleted",                     default: false
     t.string   "mailbox_type",    limit: 25
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
   end
 
   add_index "mailboxer_receipts", ["notification_id"], name: "index_mailboxer_receipts_on_notification_id", using: :btree
   add_index "mailboxer_receipts", ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type", using: :btree
 
-  create_table "pricebreaks", force: true do |t|
+  create_table "pricebreaks", force: :cascade do |t|
     t.integer  "quantity"
     t.float    "price"
     t.integer  "product_id"
@@ -175,33 +175,33 @@ ActiveRecord::Schema.define(version: 20141002211243) do
 
   add_index "pricebreaks", ["product_id"], name: "index_pricebreaks_on_product_id", using: :btree
 
-  create_table "product_categories", force: true do |t|
+  create_table "product_categories", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "products", force: true do |t|
-    t.string   "name"
-    t.string   "slug"
-    t.string   "sku"
+  create_table "products", force: :cascade do |t|
+    t.string   "name",                  limit: 255
+    t.string   "slug",                  limit: 255
+    t.string   "sku",                   limit: 255
     t.text     "description"
     t.datetime "start_date"
     t.datetime "end_date"
     t.float    "unit_price"
-    t.boolean  "status",                default: true
+    t.boolean  "status",                            default: true
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "status_action"
-    t.string   "status_characteristic"
+    t.string   "status_action",         limit: 255
+    t.string   "status_characteristic", limit: 255
     t.integer  "amount"
     t.integer  "sold_out"
-    t.string   "dimension_width"
-    t.string   "dimension_height"
-    t.string   "dimension_depth"
-    t.string   "dimension_weight"
+    t.string   "dimension_width",       limit: 255
+    t.string   "dimension_height",      limit: 255
+    t.string   "dimension_depth",       limit: 255
+    t.string   "dimension_weight",      limit: 255
     t.integer  "main_category"
     t.integer  "tax_id"
   end
@@ -209,20 +209,20 @@ ActiveRecord::Schema.define(version: 20141002211243) do
   add_index "products", ["tax_id"], name: "index_products_on_tax_id", using: :btree
   add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
 
-  create_table "requests", force: true do |t|
+  create_table "requests", force: :cascade do |t|
     t.integer  "sender_id"
     t.integer  "receiver_id"
-    t.string   "subject"
+    t.string   "subject",      limit: 255
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "conversation"
   end
 
-  create_table "roles", force: true do |t|
-    t.string   "name"
+  create_table "roles", force: :cascade do |t|
+    t.string   "name",          limit: 255
     t.integer  "resource_id"
-    t.string   "resource_type"
+    t.string   "resource_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -230,66 +230,66 @@ ActiveRecord::Schema.define(version: 20141002211243) do
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
-  create_table "taxes", force: true do |t|
-    t.string   "name"
-    t.decimal  "value",      precision: 5, scale: 2
+  create_table "taxes", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.decimal  "value",                  precision: 5, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.string   "name"
-    t.string   "phone"
-    t.string   "company"
-    t.string   "location"
-    t.string   "zip_code"
-    t.string   "city"
-    t.string   "state"
-    t.string   "country"
-    t.string   "facebook"
-    t.string   "ein_tax"
-    t.boolean  "receive_private_info",   default: true
-    t.boolean  "receive_new_offer",      default: true
-    t.boolean  "receive_tips",           default: true
-    t.string   "asset_file_name"
-    t.string   "asset_file_size"
-    t.string   "asset_content_type"
+    t.string   "unconfirmed_email",      limit: 255
+    t.string   "name",                   limit: 255
+    t.string   "phone",                  limit: 255
+    t.string   "company",                limit: 255
+    t.string   "location",               limit: 255
+    t.string   "zip_code",               limit: 255
+    t.string   "city",                   limit: 255
+    t.string   "state",                  limit: 255
+    t.string   "country",                limit: 255
+    t.string   "facebook",               limit: 255
+    t.string   "ein_tax",                limit: 255
+    t.boolean  "receive_private_info",               default: true
+    t.boolean  "receive_new_offer",                  default: true
+    t.boolean  "receive_tips",                       default: true
+    t.string   "asset_file_name",        limit: 255
+    t.string   "asset_file_size",        limit: 255
+    t.string   "asset_content_type",     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "benefits"
-    t.boolean  "is_reseller",            default: false
+    t.boolean  "is_reseller",                        default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "users_roles", id: false, force: true do |t|
+  create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
   end
 
   add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
-  create_table "versions", force: true do |t|
-    t.string   "item_type",      null: false
-    t.integer  "item_id",        null: false
-    t.string   "event",          null: false
-    t.string   "whodunnit"
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",      limit: 255, null: false
+    t.integer  "item_id",                    null: false
+    t.string   "event",          limit: 255, null: false
+    t.string   "whodunnit",      limit: 255
     t.text     "object"
     t.datetime "created_at"
     t.text     "object_changes"
@@ -297,10 +297,7 @@ ActiveRecord::Schema.define(version: 20141002211243) do
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
-  add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", name: "mb_opt_outs_on_conversations_id", column: "conversation_id"
-
-  add_foreign_key "mailboxer_notifications", "mailboxer_conversations", name: "notifications_on_conversation_id", column: "conversation_id"
-
-  add_foreign_key "mailboxer_receipts", "mailboxer_notifications", name: "receipts_on_notification_id", column: "notification_id"
-
+  add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", column: "conversation_id", name: "mb_opt_outs_on_conversations_id"
+  add_foreign_key "mailboxer_notifications", "mailboxer_conversations", column: "conversation_id", name: "notifications_on_conversation_id"
+  add_foreign_key "mailboxer_receipts", "mailboxer_notifications", column: "notification_id", name: "receipts_on_notification_id"
 end
