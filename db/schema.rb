cd90f7ce47bf21dd5bc6500e5e97f917aa4c4f8b
Brian Kung <brian@callmekung.com>
2015-03-27 20:48:53 -0500

    Updates Armor Profile fields to use bigint field type

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325165059) do
+ActiveRecord::Schema.define(version: 20150328000058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "armor_bank_accounts", force: :cascade do |t|
     t.integer  "account_type"
-    t.string   "account_location", limit: 255
-    t.string   "account_bank",     limit: 255
-    t.string   "account_routing",  limit: 255
-    t.string   "account_swift",    limit: 255
-    t.string   "account_account",  limit: 255
-    t.string   "account_iban",     limit: 255
+    t.string   "account_location"
+    t.string   "account_bank"
+    t.string   "account_routing"
+    t.string   "account_swift"
+    t.string   "account_account"
+    t.string   "account_iban"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20150325165059) do
     t.integer  "invoice_num"
     t.integer  "purchase_order_num"
     t.datetime "status_change"
-    t.string   "uri",                limit: 255
+    t.string   "uri"
     t.boolean  "deleted",                        default: false
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -74,8 +74,8 @@ ActiveRecord::Schema.define(version: 20150325165059) do
   end
 
   create_table "armor_profiles", force: :cascade do |t|
-    t.integer  "armor_account"
-    t.integer  "armor_user"
+    t.integer  "armor_account", limit: 8
+    t.integer  "armor_user",    limit: 8
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -84,15 +84,15 @@ ActiveRecord::Schema.define(version: 20150325165059) do
   add_index "armor_profiles", ["user_id"], name: "index_armor_profiles_on_user_id", using: :btree
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.integer  "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "certificates", force: :cascade do |t|
-    t.string   "filename",     limit: 255
-    t.string   "content_type", limit: 255
+    t.string   "filename"
+    t.string   "content_type"
     t.binary   "data"
     t.integer  "user_id"
     t.datetime "created_at"
@@ -103,9 +103,9 @@ ActiveRecord::Schema.define(version: 20150325165059) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "product_id"
-    t.string   "image_file_name",    limit: 255
-    t.string   "image_file_size",    limit: 255
-    t.string   "image_content_type", limit: 255
+    t.string   "image_file_name"
+    t.string   "image_file_size"
+    t.string   "image_content_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 20150325165059) do
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.integer "unsubscriber_id"
-    t.string  "unsubscriber_type", limit: 255
+    t.string  "unsubscriber_type"
     t.integer "conversation_id"
   end
 
@@ -122,26 +122,26 @@ ActiveRecord::Schema.define(version: 20150325165059) do
   add_index "mailboxer_conversation_opt_outs", ["unsubscriber_id", "unsubscriber_type"], name: "index_mailboxer_conversation_opt_outs_on_unsubscriber_id_type", using: :btree
 
   create_table "mailboxer_conversations", force: :cascade do |t|
-    t.string   "subject",    limit: 255, default: ""
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "subject",    default: ""
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "mailboxer_notifications", force: :cascade do |t|
-    t.string   "type",                 limit: 255
+    t.string   "type"
     t.text     "body"
-    t.string   "subject",              limit: 255, default: ""
+    t.string   "subject",              default: ""
     t.integer  "sender_id"
-    t.string   "sender_type",          limit: 255
+    t.string   "sender_type"
     t.integer  "conversation_id"
-    t.boolean  "draft",                            default: false
-    t.string   "notification_code",    limit: 255
+    t.boolean  "draft",                default: false
+    t.string   "notification_code"
     t.integer  "notified_object_id"
-    t.string   "notified_object_type", limit: 255
-    t.string   "attachment",           limit: 255
-    t.datetime "updated_at",                                       null: false
-    t.datetime "created_at",                                       null: false
-    t.boolean  "global",                           default: false
+    t.string   "notified_object_type"
+    t.string   "attachment"
+    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",                           null: false
+    t.boolean  "global",               default: false
     t.datetime "expires"
   end
 
@@ -152,14 +152,14 @@ ActiveRecord::Schema.define(version: 20150325165059) do
 
   create_table "mailboxer_receipts", force: :cascade do |t|
     t.integer  "receiver_id"
-    t.string   "receiver_type",   limit: 255
-    t.integer  "notification_id",                             null: false
-    t.boolean  "is_read",                     default: false
-    t.boolean  "trashed",                     default: false
-    t.boolean  "deleted",                     default: false
+    t.string   "receiver_type"
+    t.integer  "notification_id",                            null: false
+    t.boolean  "is_read",                    default: false
+    t.boolean  "trashed",                    default: false
+    t.boolean  "deleted",                    default: false
     t.string   "mailbox_type",    limit: 25
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
   end
 
   add_index "mailboxer_receipts", ["notification_id"], name: "index_mailboxer_receipts_on_notification_id", using: :btree
@@ -183,25 +183,25 @@ ActiveRecord::Schema.define(version: 20150325165059) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "name",                  limit: 255
-    t.string   "slug",                  limit: 255
-    t.string   "sku",                   limit: 255
+    t.string   "name"
+    t.string   "slug"
+    t.string   "sku"
     t.text     "description"
     t.datetime "start_date"
     t.datetime "end_date"
     t.float    "unit_price"
-    t.boolean  "status",                            default: true
+    t.boolean  "status",                default: true
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "status_action",         limit: 255
-    t.string   "status_characteristic", limit: 255
+    t.string   "status_action"
+    t.string   "status_characteristic"
     t.integer  "amount"
     t.integer  "sold_out"
-    t.string   "dimension_width",       limit: 255
-    t.string   "dimension_height",      limit: 255
-    t.string   "dimension_depth",       limit: 255
-    t.string   "dimension_weight",      limit: 255
+    t.string   "dimension_width"
+    t.string   "dimension_height"
+    t.string   "dimension_depth"
+    t.string   "dimension_weight"
     t.integer  "main_category"
     t.integer  "tax_id"
   end
@@ -212,7 +212,7 @@ ActiveRecord::Schema.define(version: 20150325165059) do
   create_table "requests", force: :cascade do |t|
     t.integer  "sender_id"
     t.integer  "receiver_id"
-    t.string   "subject",      limit: 255
+    t.string   "subject"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -220,9 +220,9 @@ ActiveRecord::Schema.define(version: 20150325165059) do
   end
 
   create_table "roles", force: :cascade do |t|
-    t.string   "name",          limit: 255
+    t.string   "name"
     t.integer  "resource_id"
-    t.string   "resource_type", limit: 255
+    t.string   "resource_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -231,47 +231,47 @@ ActiveRecord::Schema.define(version: 20150325165059) do
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
   create_table "taxes", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.decimal  "value",                  precision: 5, scale: 2
+    t.string   "name"
+    t.decimal  "value",      precision: 5, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",    null: false
-    t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
-    t.string   "name",                   limit: 255
-    t.string   "phone",                  limit: 255
-    t.string   "company",                limit: 255
-    t.string   "location",               limit: 255
-    t.string   "zip_code",               limit: 255
-    t.string   "city",                   limit: 255
-    t.string   "state",                  limit: 255
-    t.string   "country",                limit: 255
-    t.string   "facebook",               limit: 255
-    t.string   "ein_tax",                limit: 255
-    t.boolean  "receive_private_info",               default: true
-    t.boolean  "receive_new_offer",                  default: true
-    t.boolean  "receive_tips",                       default: true
-    t.string   "asset_file_name",        limit: 255
-    t.string   "asset_file_size",        limit: 255
-    t.string   "asset_content_type",     limit: 255
+    t.string   "unconfirmed_email"
+    t.string   "name"
+    t.string   "phone"
+    t.string   "company"
+    t.string   "location"
+    t.string   "zip_code"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "facebook"
+    t.string   "ein_tax"
+    t.boolean  "receive_private_info",   default: true
+    t.boolean  "receive_new_offer",      default: true
+    t.boolean  "receive_tips",           default: true
+    t.string   "asset_file_name"
+    t.string   "asset_file_size"
+    t.string   "asset_content_type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "benefits"
-    t.boolean  "is_reseller",                        default: false
+    t.boolean  "is_reseller",            default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
@@ -286,10 +286,10 @@ ActiveRecord::Schema.define(version: 20150325165059) do
   add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
   create_table "versions", force: :cascade do |t|
-    t.string   "item_type",      limit: 255, null: false
-    t.integer  "item_id",                    null: false
-    t.string   "event",          limit: 255, null: false
-    t.string   "whodunnit",      limit: 255
+    t.string   "item_type",      null: false
+    t.integer  "item_id",        null: false
+    t.string   "event",          null: false
+    t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
     t.text     "object_changes"
