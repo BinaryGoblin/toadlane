f82115b14da1e794c54c6c478a59d1540cee0cd9
Brian Kung <brian@callmekung.com>
2015-04-06 09:32:02 -0500

    Put update logic into sane place, prep for destruction of everything

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,40 @@ class User < ActiveRecord::Base
   after_save :create_armor_profile,
     if: -> { self.name && self.phone },
     unless: -> { self.armor_profile }
+  after_save :update_all_the_things,
+    if: -> { self.armor_profile }
+
+  def update_all_the_things
+    # data = ArmorService.new
+    # data.update_user({
+    #    :account => current_user.armor_profile.armor_account,
+    #   'user' => current_user.armor_profile.armor_user,
+    #   'user_name' => current_user.name,
+    #   'user_phone' => current_user.phone
+    # })
+    # data.update_company({
+    #   :account => current_user.armor_profile.armor_account,
+    #   'company' => current_user.company
+    # })
+    # data.update_address({
+    #  :account => current_user.armor_profile.armor_account,
+    #   'address' => current_user.location,
+    #   'city' => current_user.city,
+    #   'state' => current_user.state,
+    #   'postal_code' => current_user.zip_code,
+    #   'country' => current_user.country
+    # })
+    # data.create_armor_bank_account({
+    #   :account => current_user.armor_profile.armor_account,
+    #   'type' => current_user.armor_bank_account.account_type,
+    #   'location' => current_user.armor_bank_account.account_location,
+    #   'bank' => current_user.armor_bank_account.account_bank,
+    #   'routing' => current_user.armor_bank_account.account_routing,
+    #   'swift' => current_user.armor_bank_account.account_swift,
+    #   'account' => current_user.armor_bank_account.account_account,
+    #   'iban' => current_user.armor_bank_account.account_iban
+    # })
+  end
 
   def role
     self.roles.all.first
