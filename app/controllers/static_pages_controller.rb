e961a776a35a6729888b78172b2b3cb55bda2ca6
Brian Kung <brian@callmekung.com>
2015-05-24 17:36:55 -0500

    Uses previous layouts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,18 @@
 class StaticPagesController < ApplicationController
-  def contact_info;end
-  def payment_info;end
+  layout 'user_dashboard'
+
+  def contact_info
+    render 'static_pages/contact_info'
+  end
+
+  def payment_info
+    render 'static_pages/payment_info'
+  end
+
   def faq;end
 
   def home
     redirect_to products_path if current_user
+    render 'static_pages/home', layout: 'application'
   end
 end
