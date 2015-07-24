efd93c113531b7fa46039c77e180a2966abf4d60
joeliuscaeser@gmail.com <joeliuscaeser@gmail.com>
2015-07-24 12:15:03 -0700

    Revamping the product display page to differentiate between buy and sell types and changing the carousels on the main product page   - Added css for sell type carousels   - Added routes for for_sale products and requested products   - Added for_sale and requested methods to product controller   - Changed the carousels on the product index page to recommended, for_sale, and requested   - Added visual style to carousel on requested listings

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,13 +3,13 @@ class ProductsController < ApplicationController
 
   before_action :set_product, only: [:show]
 
-  before_filter :authenticate_user!, except: [:index, :products, :show]
+  before_filter :authenticate_user!, except: [:index, :products, :show, :for_sale, :requested]
   before_action :check_terms_of_service
 
   def index
     @products_recommended = Product.unexpired.where(status_action: 'recommended').order(:created_at).limit(16)
-    @products_best = Product.unexpired.where(status_action: 'best').order(:created_at).limit(16)
-    @products_new_deals = Product.unexpired.order(:created_at).limit(16)
+    @products_for_sale = Product.unexpired.where(status_characteristic: 'sell').order(:created_at).limit(16)
+    @products_requested = Product.unexpired.where(status_characteristic: 'buy').order(:created_at).limit(16)
     @featured_sellers = User.limit(16)
   end
 
@@ -21,13 +21,18 @@ class ProductsController < ApplicationController
     @products = Product.unexpired.order('updated_at DESC').paginate(page: params[:page], per_page: params[:count]).order('id DESC')
   end
 
-  def offers
+  def deals
+# TODO
+  end
+  
+  def for_sale
     @products = Product.unexpired.where(status_characteristic: 'sell').paginate(page: params[:page], per_page: params[:count]).order('id DESC')
     render 'products/products'
   end
-
-  def deals
-# TODO
+  
+  def requested
+    @products = Product.unexpired.where(status_characteristic: 'buy').paginate(page: params[:page], per_page: params[:count]).order('id DESC')
+    render 'products/products'
   end
 
   private
