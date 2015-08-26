e8c7dd958659ff5b86ee5eb9f7930ded4cb2e3f7
joeliuscaeser@gmail.com <joeliuscaeser@gmail.com>
2015-08-26 03:19:37 -0700

    Implementing the financing application form
      - Adding fanancing application form tab to dashboard
      - Adding financing mailer

---
+++ app/mailers/finances_mailer.rb
@@ -0,0 +1,11 @@
+class FinancesMailer < ActionMailer::Base
+  default from: "hello@toadlane.com"
+
+  def multivision_application_email(user, loan_amount, business_years, monthly_revenue)
+    @user = user
+    @loan_amount = loan_amount
+    @business_years = business_years
+    @monthly_revenue = monthly_revenue
+    mail to: 'admin@multivisionfinancial.com', subject: 'New loan request from Toadlane'
+  end
+end
