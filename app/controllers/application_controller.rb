45fabf5c4c357e0522ef98a7fb29a15a42cf48d2
joeliuscaeser@gmail.com <joeliuscaeser@gmail.com>
2016-03-11 02:32:20 -0800

    Adding custom mobile views
      - Adding views_mobile directory
      - Adding filters and helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,4 +67,22 @@ class ApplicationController < ActionController::Base
       super
     end
   end
+  
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+    prepare_for_mobile if mobile_device?
+  end
+  
+  def mobile_device?
+    if session[:mobile_override]
+      session[:mobile_override] == "1"
+    else
+      (request.user_agent =~ /iPhone|iPod|Android|Mobile|webOS/) && (request.user_agent !~ /iPad/)
+    end
+  end
+  helper_method :mobile_device?
+  
+  def prepare_for_mobile
+    prepend_view_path Rails.root + 'app' + 'views_mobile'
+  end
 end
