7738d0933ef54b37ca0ab143fc7b7207a9601fc2
joeliuscaeser@gmail.com <joeliuscaeser@gmail.com>
2016-03-14 22:26:42 -0700

    Setting Stripe up as the only payment gateway.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @fee = Fee.all()[0].value
+    @fee = Fee.find_by(:module_name => "Stripe").value
     @related_products = Product.unexpired.where(main_category: @product.main_category).where.not(id: @product.id)
   end
 
