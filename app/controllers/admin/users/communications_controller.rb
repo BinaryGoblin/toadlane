cf22ebdee71f1655f603683765544e00f1c54a24
Sushil Shrestha <sushil10018@gmail.com>
2016-05-13 15:13:53 +0545

    admin to use the same session controller as user

--- app/controllers/admin/users/communications_controller.rb
+++ app/controllers/admin/users/communications_controller.rb
@@ -1,4 +1,6 @@
 class Admin::Users::CommunicationsController < Admin::UsersController
+  layout 'admin_dashboard'
+  
   def index
     @users = User.paginate(page: params[:page], per_page: params[:count]).order('id ASC')
   end
