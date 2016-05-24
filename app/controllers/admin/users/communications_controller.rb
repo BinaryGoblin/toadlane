47b5f34f2f2b773ff1ca53a00f7a9fd7882d8164
Sushil Shrestha <sushil10018@gmail.com>
2016-05-24 16:38:17 +0545

    Fixed the issue with layout in admin > User Controller which fixed for other sub-controllers.

--- app/controllers/admin/users/communications_controller.rb
+++ app/controllers/admin/users/communications_controller.rb
@@ -1,5 +1,4 @@
 class Admin::Users::CommunicationsController < Admin::UsersController
-  layout 'admin_dashboard'
   
   def index
     @users = User.paginate(page: params[:page], per_page: params[:count]).order('id ASC')
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < Admin::ApplicationController
+  
+  layout 'admin_dashboard'
+
   private
     def set_user
       @user = User.find(params[:id])
