11885e8dd2f4bb8d4cd429d7967d2c7b2bb80619
NEHA SUWAl <neha@jyaasa.com>
2016-06-15 13:59:45 +0545

    Enabled the subscription thread so that after a user comments an email notification is sent

--- config/initializers/commontator.rb
+++ config/initializers/commontator.rb
@@ -208,7 +208,7 @@ Commontator.configure do |config|
   #   :m (manual subscriptions only)
   #   :b (both automatic, when commenting, and manual)
   # Default: :n
-  config.thread_subscription = :n
+  config.thread_subscription = :a
 
   # email_from_proc
   # Type: Proc
