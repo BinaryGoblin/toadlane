3fcaa5c2133e5b7d401640e38ff7a6665b8b7927
NEHA SUWAl <neha@jyaasa.com>
2016-06-16 17:25:09 +0545

    Directly used the missing.png file for if the user has no uploaded image

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -2,11 +2,15 @@ module EmailHelper
 
   def email_image_tag(image)
     if image.split("/").include?("missing.png")
-      attachments.inline[image] = File.read(Rails.root.join("app/assets/images/avatar/small/missing.png"))
+      if Rails.env == "development"
+        attachments.inline[image] = File.read(Rails.root.join("app/assets/images/avatar/small/missing.png"))
+        image_tag attachments.inline[image].url
+      else
+        image_tag "missing.png"
+      end
     else
       attachments.inline[image] = File.read(Rails.root.join("public#{image}"))
+      image_tag attachments.inline[image].url
     end
-
-    image_tag attachments.inline[image].url
   end
 end
