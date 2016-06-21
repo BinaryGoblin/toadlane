b5c15e697ab34fc6bf9a64c2145bdd8c27b3a62f
NEHA SUWAl <neha@jyaasa.com>
2016-06-21 16:16:07 +0545

    - Modified the email template for sending email notification when sales occur for both buyer and seller

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,6 @@
 module EmailHelper
 
-  def get_user_image(image_path)
+  def get_image(image_path)
     attachments.inline[image_path] = open(image_path).read
     attachments[image_path].url
   end
@@ -11,5 +11,9 @@ module EmailHelper
     else
       user.email
     end
+  end
+
+  def get_amount_without_fees(stripe_order)
+    stripe_order.unit_price * stripe_order.count
   end
 end
