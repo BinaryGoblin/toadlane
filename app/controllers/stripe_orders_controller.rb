442abaf7b8a420416d452294f55a0386c9671124
NEHA SUWAl <neha@jyaasa.com>
2016-06-21 10:29:47 +0545

    Email notification after sales is done 	- Added two methods in user_mailer.rb for sending email to buyer or seller 	- Respectively, added templates for both to buyer and to seller 	- Added helper method for getting user's name or email 	- Called those two methods after order is placed

--- app/controllers/stripe_orders_controller.rb
+++ app/controllers/stripe_orders_controller.rb
@@ -1,16 +1,16 @@
 class StripeOrdersController < ApplicationController
   before_filter :authenticate_user!
   before_action :check_terms_of_service
-  
+
   def show
     @stripe_order = StripeOrder.find(params[:id])
   end
- 
+
   def create
     @stripe_order = StripeOrder.new(stripe_order_params)
     @stripe_order.save
     @stripe_order.start_stripe_order(stripe_params["stripeToken"])
-    
+
     if stripe_order_params[:address_id] == "-1"
       address = Address.new
       address.name = stripe_params["stripeShippingName"]
@@ -21,23 +21,26 @@ class StripeOrdersController < ApplicationController
       address.city = stripe_params["stripeShippingAddressCity"]
       address.country = stripe_params["stripeShippingAddressCountry"]
       address.user = @stripe_order.buyer
-      
+
       @stripe_order.address = address
     end
-    
+
     @stripe_order.calculate_shipping()
-      
+
     @stripe_order.process_payment()
+
+    UserMailer.sales_order_notification_to_seller(@stripe_order).deliver
+    UserMailer.sales_order_notification_to_buyer(@stripe_order).deliver
     redirect_to dashboard_order_path(@stripe_order, :type => "stripe"), notice: "Your order was succesfully placed."
   end
-  
+
   private
     def stripe_order_params
       params.require(:stripe_order).permit(:id, :buyer_id, :seller_id, :product_id, :stripe_charge_id, :status, :unit_price, :count, :fee, :rebate, :total, :summary,
                                            :description, :shipping_address, :shipping_request, :shipping_details, :tracking_number, :deleted, :shipping_cost,
                                            :address_name, :address_city, :address_state, :address_country, :address_zip, :address_id, :shipping_estimate_id)
     end
-    
+
     def stripe_params
       params.permit(:stripeToken, :stripeEmail, :stripeShippingName, :stripeShippingAddressLine1, :stripeShippingAddressLine2,
                     :stripeShippingAddressZip, :stripeShippingAddressState, :stripeShippingAddressCity, :stripeShippingAddressCountry)
--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -4,4 +4,12 @@ module EmailHelper
     attachments.inline[image_path] = open(image_path).read
     attachments[image_path].url
   end
+
+  def get_email_or_name(user)
+    if user.name.present?
+      user.name
+    else
+      user.email
+    end
+  end
 end
