61ce61be404ea2a48a660ffc00acd9049c43a542
Sushil Shrestha <sushil10018@gmail.com>
2016-06-22 10:50:34 +0545

    Generate GreenOrder Model with associations

---
+++ app/models/green_order.rb
@@ -0,0 +1,14 @@
+class GreenOrder < ActiveRecord::Base
+  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
+  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
+  belongs_to :product
+  belongs_to :shipping_estimate
+  belongs_to :address
+
+  scope :for_dashboard, -> (page, per_page) do
+    where(deleted: false).order('created_at DESC').paginate(page: page, per_page: per_page)
+  end
+
+  # not_started must be first (ie. at index 0) for the default value to be correct
+  enum status: [ :not_started, :started, :shipping_estimate, :cancelled, :placed, :shipped, :completed, :challenged, :refunded ]
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617095747) do
+ActiveRecord::Schema.define(version: 20160621111336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,6 +164,34 @@ ActiveRecord::Schema.define(version: 20160617095747) do
     t.decimal  "value",       precision: 5, scale: 3
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+  end
+
+  create_table "green_orders", force: :cascade do |t|
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
+    t.integer  "product_id"
+    t.string   "check_number"
+    t.string   "check_id"
+    t.integer  "status",                           default: 0
+    t.float    "unit_price"
+    t.integer  "count"
+    t.float    "fee"
+    t.float    "rebate"
+    t.float    "total"
+    t.string   "summary",              limit: 100
+    t.text     "description"
+    t.string   "tracking_number"
+    t.boolean  "deleted",                          default: false, null: false
+    t.float    "shipping_cost"
+    t.string   "address_name"
+    t.string   "address_city"
+    t.string   "address_state"
+    t.string   "address_zip"
+    t.string   "address_country"
+    t.integer  "shipping_estimate_id"
+    t.integer  "address_id"
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
   end
 
   create_table "green_profiles", force: :cascade do |t|
