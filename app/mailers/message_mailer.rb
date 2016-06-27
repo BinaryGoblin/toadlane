2daf953b0b58b95c208a516247d854ff0bf94929
NEHA SUWAl <neha@jyaasa.com>
2016-06-27 11:20:20 +0545

    Added different reply_to for production env

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -8,8 +8,15 @@ class MessageMailer < ActionMailer::Base
     @subject = subject
     @sender = sender
     @message_url = dashboard_message_url(conversation_id)
+
+    if Rails.env.production?
+      reply_to = "dcffb0d65f0f2b20786b5d8056648023+#{conversation_id}@inbound.postmarkapp.com"
+    else
+      reply_to = "c8b7ddb781077404c27659d9ef85d353+#{conversation_id}@inbound.postmarkapp.com"
+    end
+
     mail( to: @receiver.email,
           subject: "New Message from #{@sender.name || @sender.email}",
-          reply_to: "c8b7ddb781077404c27659d9ef85d353+#{conversation_id}@inbound.postmarkapp.com")
+          reply_to: reply_to)
   end
 end
