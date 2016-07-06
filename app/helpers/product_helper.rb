0292836901b32a127f7bc6506f08a17b87033a52
NEHA SUWAl <neha@jyaasa.com>
2016-07-06 17:15:44 +0545

    Calcualtion for subtotal

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -3,4 +3,8 @@ module ProductHelper
   def get_shipping_cost(quantity, shipping_cost)
     quantity.to_i * shipping_cost.to_f
   end
+
+  def get_subtotal(unit_price, quantity)
+    quantity.to_i * unit_price
+  end
 end
