6aeb667fff09f0dce4e0875524709ff36c87a99b
NEHA SUWAl <neha@jyaasa.com>
2016-07-21 14:19:05 +0545

    Removed unnecessary codes

--- app/controllers/armor_orders_controller.rb
+++ app/controllers/armor_orders_controller.rb
@@ -20,7 +20,7 @@ class ArmorOrdersController < ApplicationController
 
   def create
     product = Product.unexpired.find(armor_order_params[:product_id])
-    
+
     additional_params = {
       buyer_id: current_user.id,
       seller_id: product.user.id,
@@ -52,48 +52,6 @@ class ArmorOrdersController < ApplicationController
     else
       redirect_to :back, :flash => { :alert => @armor_order.errors.messages}
     end
-    # product = Product.unexpired.find(armor_order_params[:product_id])
-    #
-    # if product.status_characteristic == 'sell'
-    #   additional_params = {
-    #     buyer_id: current_user.id,
-    #     seller_id: product.user.id,
-    #     product_id: product.id,
-    #     status_change: DateTime.now,
-    #     account_id: current_user.armor_account_id
-    #   }
-    # else
-    #   additional_params = {
-    #     buyer_id: product.user.id,
-    #     seller_id: current_user.id,
-    #     product_id: product.id,
-    #     status_change: DateTime.now,
-    #     account_id: current_user.armor_account_id
-    #   }
-    # end
-    #
-    # @armor_order = ArmorOrder.new(armor_order_params.merge(additional_params))
-    #
-    # respond_to do |format|
-    #   if @armor_order.save
-    #
-    #     api_armor_order_params = {
-    #       'seller_id'   => "#{product.user.armor_user_id}",
-    #       'buyer_id'    => "#{current_user.armor_user_id}",
-    #       'amount'      => armor_order_params["amount"],
-    #       'summary'     => product.name,
-    #       'description' => product.description
-    #     }
-    #
-    #     @armor_order.create_armor_api_order(current_user.armor_account_id, api_armor_order_params)
-    #
-    #     format.html { redirect_to dashboard_orders_path, notice: 'Armor Order was successfully created.' }
-    #     format.json { render :show, status: :created, location: @armor_order }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @armor_order.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   def update
