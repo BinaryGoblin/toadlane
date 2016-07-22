6db02520f2ebadeb77a3de0197e4d49afd8716e1
NEHA SUWAl <neha@jyaasa.com>
2016-07-22 11:25:47 +0545

    - Added field inspection_date to product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721074231) do
+ActiveRecord::Schema.define(version: 20160722054007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -384,6 +384,7 @@ ActiveRecord::Schema.define(version: 20160721074231) do
     t.integer  "type",                            default: 0
     t.integer  "views_count",                     default: 0
     t.datetime "deleted_at"
+    t.datetime "inspection_date"
   end
 
   add_index "products", ["deleted_at"], name: "index_products_on_deleted_at", using: :btree
