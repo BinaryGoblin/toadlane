a07a71ecccd785c78f2eb2a07a5abb9540989a54
NEHA SUWAl <neha@jyaasa.com>
2016-08-10 17:08:19 +0545

    work in progress for validating inspection dates

--- app/models/inspection_date.rb
+++ app/models/inspection_date.rb
@@ -21,8 +21,25 @@ class InspectionDate < ActiveRecord::Base
   scope :seller_added, -> { where(creator_type: "seller") }
   scope :buyer_added, -> { where(creator_type: "buyer") }
 
+  validate :inspection_date_validator, :on => :create
+
   # gives August 09, 2016, 06:00 PM
   def get_inspection_date
     date.strftime("%B %d, %Y, %I:%M %p")
+  end
+
+  def inspection_date_validator
+    binding.pry
+    if date.present?
+      product = Product.find_by_id(self.product_id)
+      previous_added_date = product.inspection_dates.select{ |inspection_date| inspection_date.date.to_date == date.to_date}
+      if previous_added_date.any?
+        errors.add(:date, 'must be greater than today.')
+      else
+        if date.to_date <= Date.today
+          errors.add(:date, 'must be greater than today.')
+        end
+      end
+    end
   end
 end
