44f7897ccb710896261297724a5e9d625dcda70a
NEHA SUWAl <neha@jyaasa.com>
2016-08-29 09:38:56 +0545

    managed alignment

--- app/controllers/dashboard/notifications_controller.rb
+++ app/controllers/dashboard/notifications_controller.rb
@@ -1,6 +1,8 @@
 class Dashboard::NotificationsController < DashboardController
   def index
-    @notifications = Kaminari.paginate_array(current_user.notifications.order('created_at DESC')).page(params[:page]).per(5)
+    @notifications = Kaminari.paginate_array(
+      current_user.notifications.order('created_at DESC')
+    ).page(params[:page]).per(5)
 
     if current_user.notifications.not_marked_read.present?
       mark_as_read
