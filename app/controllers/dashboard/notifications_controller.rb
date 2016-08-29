128e8f6c627c986137554b67614bd822bbd64d02
NEHA SUWAl <neha@jyaasa.com>
2016-08-29 09:34:31 +0545

    added pagination

--- app/controllers/dashboard/notifications_controller.rb
+++ app/controllers/dashboard/notifications_controller.rb
@@ -1,12 +1,15 @@
 class Dashboard::NotificationsController < DashboardController
   def index
-    @notifications = current_user.notifications.order('created_at DESC')
-    mark_as_read
+    @notifications = Kaminari.paginate_array(current_user.notifications.order('created_at DESC')).page(params[:page]).per(5)
+
+    if current_user.notifications.not_marked_read.present?
+      mark_as_read
+    end
   end
 
   private
   def mark_as_read
-    current_user.notifications.each do |notification|
+    current_user.notifications.not_marked_read.each do |notification|
       notification.update_attribute(:read, true)
     end
   end
