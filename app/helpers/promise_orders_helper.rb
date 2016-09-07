3c30b9b885c35098a15fc37e307555fc37dba838
NEHA SUWAl <neha@jyaasa.com>
2016-09-07 17:28:28 +0545

    added condition

--- app/helpers/promise_orders_helper.rb
+++ app/helpers/promise_orders_helper.rb
@@ -1,7 +1,9 @@
 module PromiseOrdersHelper
   def get_seller_charged_fee(order)
-    number_with_precision(order.transaction_fee_amount + order.fraud_protection_fee_amount +
-                        order.end_user_support_fee_amount, :precision => 2)
+    if order.transaction_fee_amount.present? && order.fraud_protection_fee_amount.present? && order.end_user_support_fee_amount.present?
+      number_with_precision(order.transaction_fee_amount + order.fraud_protection_fee_amount +
+                          order.end_user_support_fee_amount, :precision => 2)
+    end
   end
 
   def get_order_amount_for_seller(order)
