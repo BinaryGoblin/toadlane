954d67f11dfd0fbf9310504bea688e8a23618c92
NEHA SUWAl <neha@jyaasa.com>
2016-09-08 10:01:26 +0545

    if the default payment is 'Fly and Buy', then only validate inspection date

--- app/models/inspection_date.rb
+++ app/models/inspection_date.rb
@@ -30,21 +30,22 @@ class InspectionDate < ActiveRecord::Base
 
   def check_inspection_date
     product = Product.find_by_id(product_id)
+    if product.default_payment == 'Fly And Buy'
+      if creator_type == "seller"
+        existing_dates_except_self = product.inspection_dates.seller_added
+        .where.not(id: id)
+        .where('date BETWEEN ? AND ?', date.beginning_of_day, date.end_of_day)
+      else
+        existing_dates_except_self = product.inspection_dates.where.not(id: id)
+        .where('date BETWEEN ? AND ?', date.beginning_of_day, date.end_of_day )
+      end
 
-    if creator_type == "seller"
-      existing_dates_except_self = product.inspection_dates.seller_added
-      .where.not(id: id)
-      .where('date BETWEEN ? AND ?', date.beginning_of_day, date.end_of_day)
-    else
-      existing_dates_except_self = product.inspection_dates.where.not(id: id)
-      .where('date BETWEEN ? AND ?', date.beginning_of_day, date.end_of_day )
-    end
-
-    if existing_dates_except_self.any?
-      errors.add(:date, 'must be unique.')
-    end
-    if date.to_date <= Date.today
-      errors.add(:date, 'must be greater than today.')
+      if existing_dates_except_self.any?
+        errors.add(:date, 'must be unique.')
+      end
+      if date.to_date <= Date.today
+        errors.add(:date, 'must be greater than today.')
+      end
     end
   end
 end
