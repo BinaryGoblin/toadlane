50cbb2a24e5b010f08303924dadcc7062aeedea5
NEHA SUWAl <neha@jyaasa.com>
2016-09-28 15:28:47 +0545

    Retrieved client id and client secret as per env

--- lib/fly_buy_service.rb
+++ lib/fly_buy_service.rb
@@ -7,11 +7,19 @@ class FlyBuyService
       sandbox_mode = true
     end
 
+    if Rails.env.development?
+      client_id = Rails.application.secrets['SYNAPSEPAY_CLIENT_ID']
+      client_secret = Rails.application.secrets['SYNAPSEPAY_CLIENT_SECRET']
+    else
+      client_id = ENV['SYNAPSEPAY_CLIENT_ID']
+      client_secret = ENV['SYNAPSEPAY_CLIENT_SECRET']
+    end
+
     timeout_options = { write: 50, connect: 50, read: 50 }
 
     SynapsePayments::Client.new(
-                        client_id: Rails.application.secrets['SYNAPSEPAY_CLIENT_ID'],
-                        client_secret: Rails.application.secrets['SYNAPSEPAY_CLIENT_SECRET'],
+                        client_id: client_id,
+                        client_secret: client_secret,
                         sandbox_mode: sandbox_mode, 
                         timeout_options: timeout_options)
 
