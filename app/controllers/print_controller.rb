170753d4bc2e27ae5fc9390dadd07353d3f19c35
NEHA SUWAl <neha@jyaasa.com>
2016-09-29 15:41:29 +0545

    added type for printing invoice

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -7,10 +7,31 @@ class PrintController < ApplicationController
     case params[:type]
     when 'stripe'
       @order = StripeOrder.find(params[:id])
-    when 'armor'
-      @order = ArmorOrder.find(params[:id])
+    when 'green'
+      @order = GreenOrder.find(params[:id])
+    when 'amg'
+      @order = AmgOrder.find(params[:id])
+    when 'emb'
+      @order = EmbOrder.find(params[:id])
+    when 'fly_buy'
+      @order = FlyBuyOrder.find(params[:id])
     else
       @order = StripeOrder.find(params[:id])
     end      
+  end
+
+  def print_result_pdf(result)
+    target_partial_path = Rails.root + '/app/views/drug_tests/_print_result.pdf.slim'
+    pdf_template_path = 'layouts/pdf.slim'
+    file_name = "print_result_#{Time.now.to_i}.pdf"
+    pdf = render_to_string(
+        {
+            layout: pdf_template_path,
+            file: target_partial_path,
+            pdf: file_name,
+            locals: {result: result}, margin: {top: "1.0mm"}
+        })
+
+    send_data pdf, filename: file_name
   end
 end
