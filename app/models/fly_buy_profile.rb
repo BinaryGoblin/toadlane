26d293b3b6a93778c2a88aeb990604c969b5a94e
NEHA SUWAl <neha@jyaasa.com>
2016-10-07 10:12:37 +0545

    Added field entity_type and entity_scope

--- app/models/fly_buy_profile.rb
+++ app/models/fly_buy_profile.rb
@@ -20,7 +20,8 @@
 class FlyBuyProfile < ActiveRecord::Base
   belongs_to :user
 
-  attr_accessor :name_on_account, :account_num, :routing_num, :bank_name, :address, :ssn_number, :date_of_company
+  attr_accessor :name_on_account, :account_num, :routing_num, :bank_name, :address,
+  								:ssn_number, :date_of_company, :entity_type, :entity_scope
 
   EscrowNodeID = '57d7465386c2732e824b7c8b'
   AppUserId = '57d745ff86c27319cbe0edf0'
