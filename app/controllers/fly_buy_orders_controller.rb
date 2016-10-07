2d9bc54336dc733a6486f1563b0a86aa242c3c94
NEHA SUWAl <neha@jyaasa.com>
2016-10-07 16:23:27 +0545

    added redirect url and removed binding

--- app/controllers/fly_buy_orders_controller.rb
+++ app/controllers/fly_buy_orders_controller.rb
@@ -268,9 +268,13 @@ class FlyBuyOrdersController < ApplicationController
     cancel_transaction = client_user.trans.delete(
                           node_id: buyer_fly_buy_profile.synapse_node_id,
                           trans_id: fly_buy_order.synapse_transaction_id)
-    binding.pry
+
     if cancel_transaction["recent_status"]["status"] == "CANCELED"
       fly_buy_order.update_attribute(:status, 'cancelled')
+      redirect_to dashboard_order_path(
+        fly_buy_order,
+        type: 'fly_buy'
+      ), notice: 'You order has been canceled.'
     end 
   end
 
