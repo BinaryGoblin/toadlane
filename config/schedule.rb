4fbfb559b756f40c9d5a6817d161bf5f03bdc5c5
NEHA SUWAl <neha@jyaasa.com>
2016-10-07 16:52:33 +0545

    added rake task to scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,4 +22,5 @@ every :day, :at => '12:01am' do
   rake "reminder_inspection_date_arriving"
   rake "on_inspection_date_order_status_change"
   rake "email_notification_buyer_send_funds"
+  rake "cancel_transaction_funds_not_received"
 end
