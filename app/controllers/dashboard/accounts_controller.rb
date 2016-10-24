1c06f4015cf0f71f35ab8510c48c8fc3fc6c93d8
NEHA SUWAl <neha@jyaasa.com>
2016-10-24 10:52:50 +0545

    after entered the details of the form saved in flybuyprofile table so that not have to enteer it again

--- app/controllers/dashboard/accounts_controller.rb
+++ app/controllers/dashboard/accounts_controller.rb
@@ -26,30 +26,30 @@ class Dashboard::AccountsController < DashboardController
   end
 
   def create_fly_buy_profile
-    # if current_user.fly_buy_profile.present?
-    #   fly_buy_profile = FlyBuyProfile.where(user_id: current_user.id)
-    # else
-    #   necessary_fly_buy_params = fly_buy_params.except(:fingerprint, :bank_name, :address, :name_on_account, :account_num)
-    #   necessary_fly_buy_params.merge!(synapse_ip_address: request.ip, encrypted_fingerprint: "user_#{current_user.id}" + "_" + fly_buy_params["fingerprint"], user_id: current_user.id)
-    #   fly_buy_profile = FlyBuyProfile.create(necessary_fly_buy_params)
-    #   address_params = params["addresses"].merge!(name: current_user.company, of_company: true)
-    #   current_user.addresses.create(address_params)
-    # end
+    if current_user.fly_buy_profile.present?
+      fly_buy_profile = FlyBuyProfile.where(user_id: current_user.id)
+    else
+      necessary_fly_buy_params = fly_buy_params.except(:address_id, :fingerprint, :bank_name, :address, :name_on_account, :account_num)
+      necessary_fly_buy_params.merge!(synapse_ip_address: request.ip, encrypted_fingerprint: "user_#{current_user.id}" + "_" + fly_buy_params["fingerprint"], user_id: current_user.id)
+      fly_buy_profile = FlyBuyProfile.create(necessary_fly_buy_params)
+    end
     
     if current_user.present? && current_user.profile_complete? == false
       return redirect_to dashboard_accounts_path, :flash => { :account_error => "You must complete your profile before you can create a bank account." }
     end
 
     if current_user.present? && current_user.profile_complete? && current_user.name.present? && current_user.name.count(" ") == 0
-      return redirect_to dashboard_accounts_path, :flash => { :account_error => "You must have your full name before adding bank account." }
-    end
-
-    if current_user.present? && current_user.profile_complete? && current_user.company.present? && current_user.company.count(" ") == 0
       return redirect_to dashboard_accounts_path, :flash => { :account_error => "You must update your first and last name prior to submitting your company information" }
+    end
+    # if current_user.present? && current_user.profile_complete? && current_user.company.present? && current_user.company.count(" ") == 0
+    #   return redirect_to dashboard_accounts_path, :flash => { :account_error => "You must update your first and last name prior to submitting your company information" }
+    # end
+
+    if current_user.present? && current_user.profile_complete? && current_user.company.present? == false
+      return redirect_to dashboard_accounts_path, :flash => { :account_error => "You must add your company name prior to submitting your company information." }
     end
 
     if request.post? && fly_buy_params.present? && params["addresses"].present?
-      fly_buy_params.merge!(ip_address: request.ip)
       FlyAndBuy::UserOperations.new(current_user, fly_buy_params).create_user
 
       FlyAndBuy::AddingBankDetails.new(current_user, current_user.fly_buy_profile, fly_buy_params).add_details
