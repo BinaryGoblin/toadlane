2f1690ff7f2d5fc2a420752cf12a0fb6a28b7bf9
NEHA SUWAl <neha@jyaasa.com>
2016-10-26 16:16:13 +0545

    added only flybuyprofile.present?

--- app/models/user.rb
+++ app/models/user.rb
@@ -239,7 +239,7 @@ class User < ActiveRecord::Base
   end
 
   def fly_buy_profile_account_added?
-    fly_buy_profile_exist? && fly_buy_profile.completed == true
+    fly_buy_profile.present? && fly_buy_profile.completed == true
   end
 
   private
