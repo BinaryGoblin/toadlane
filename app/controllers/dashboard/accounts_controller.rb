6f280f89029269bfb33581ecc4a1e0f352aeae9c
NEHA SUWAl <neha@jyaasa.com>
2016-10-26 15:52:59 +0545

    corrected typo

--- app/controllers/dashboard/accounts_controller.rb
+++ app/controllers/dashboard/accounts_controller.rb
@@ -187,7 +187,7 @@ class Dashboard::AccountsController < DashboardController
           })
           UserMailer.send_account_verified_notification_to_user(fly_buy_profile).deliver_later
         else
-          UserMailer.send_account_not_verified_yet_notification_to_user.(fly_buy_profile).deliver_later
+          UserMailer.send_account_not_verified_yet_notification_to_user(fly_buy_profile).deliver_later
         end
       end
     end
