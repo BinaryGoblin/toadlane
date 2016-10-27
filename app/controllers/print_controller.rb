a704f053a56c464b1d521b4cb0d9f67e2edfc441
NEHA SUWAl <neha@jyaasa.com>
2016-10-27 12:13:10 +0545

    removed method

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -19,25 +19,4 @@ class PrintController < ApplicationController
       @order = StripeOrder.find(params[:id])
     end      
   end
-
-  def print_result_pdf(result)
-    html = ac.render_to_string(
-        {
-            layout: 'layouts/print.html.slim',
-            file: Rails.root + '/app/views/shared/_invoice.html.slim',
-            locals: {order: FlyBuyOrder.last}
-        })
-    kit = IMGKit.new(ac.render_to_string(
-        {
-            layout: 'layouts/print.html.slim',
-            file: Rails.root + '/app/views/shared/_invoice.html.slim',
-            locals: {order: FlyBuyOrder.last}
-        }))
-
-    img   = kit.to_img(:png)
-    file  = Tempfile.new(["template_#{FlyBuyOrder.last}", 'png'], 'tmp',
-                         :encoding => 'ascii-8bit')
-    file.write(img)
-
-  end
 end
