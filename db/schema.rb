86388db850a81a3a40df07cb85a75f12a4f75969
Chiranjib Roy <raychiranjib1@gmail.com>
2017-03-21 22:55:58 +0530

    f20170321241-search_bar_issue
    UI for desktop and laptop.

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@
 ActiveRecord::Schema.define(version: 20170321080132) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "additional_seller_fees", force: :cascade do |t|
@@ -441,14 +440,15 @@ ActiveRecord::Schema.define(version: 20170321080132) do
   create_table "group_sellers", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "product_id"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.boolean  "accept_deal"
     t.integer  "group_id"
-    t.boolean  "private_seller", default: false
-    t.integer  "role_id"
+    t.boolean  "private_seller",        default: false
+    t.decimal  "additional_seller_fee"
     t.decimal  "fee"
-    t.boolean  "notified",       default: false
+    t.integer  "role_id"
+    t.boolean  "notified",              default: false
   end
 
   create_table "groups", force: :cascade do |t|
