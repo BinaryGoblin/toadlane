84287deea9e4b6f35b4ed1ab308f8a85572a81e1
Diganta Mandal <diganta@codeapogee.com>
2017-04-06 18:49:12 +0530

    b20170406268-invoice_doesnot_seem_to_calculate_total_amount_for_seller
    * Fixed indentation issue in group saller model.

--- app/models/group_seller.rb
+++ app/models/group_seller.rb
@@ -11,39 +11,39 @@
 #  group_id       :integer
 #  fee            :decimal(, )
 #  private_seller :boolean          default(FALSE)
-#  role_id				:integer
+#  role_id        :integer
 #
 
 class GroupSeller < ActiveRecord::Base
-	#
-	## private_seller => this field is defaulted as false, the user can set himself as private seller,
-	### this will not show the user in the group.
-	belongs_to :user
-	belongs_to :product
-	belongs_to :group
-	belongs_to :role
+  #
+  ## private_seller => this field is defaulted as false, the user can set himself as private seller,
+  ### this will not show the user in the group.
+  belongs_to :user
+  belongs_to :product
+  belongs_to :group
+  belongs_to :role
 
-	scope :deal_accepted, -> { where(accept_deal: true) }
-	scope :private_members, -> { joins(:role).where('roles.name = ? or roles.name = ?', Role::PRIVATE_SELLER, Role::PRIVATE_SUPPLIER) }
+  scope :deal_accepted, -> { where(accept_deal: true) }
+  scope :private_members, -> { joins(:role).where('roles.name = ? or roles.name = ?', Role::PRIVATE_SELLER, Role::PRIVATE_SUPPLIER) }
   scope :public_members, -> { joins(:role).where('roles.name = ? or roles.name = ?', Role::PUBLIC_SELLER, Role::PUBLIC_SUPPLIER) }
 
-	def profile_complete?
-		user.profile_complete?
-	end
+  def profile_complete?
+    user.profile_complete?
+  end
 
-	def fly_buy_profile_verified?
-		user.fly_buy_profile_verified?
-	end
+  def fly_buy_profile_verified?
+    user.fly_buy_profile_verified?
+  end
 
-	def fly_buy_profile_account_added?
-		user.fly_buy_profile_account_added?
-	end
+  def fly_buy_profile_account_added?
+    user.fly_buy_profile_account_added?
+  end
 
-	def private_member?
-		self.role.name == Role::PRIVATE_SELLER || self.role.name == Role::PRIVATE_SUPPLIER
-	end
+  def private_member?
+    self.role.name == Role::PRIVATE_SELLER || self.role.name == Role::PRIVATE_SUPPLIER
+  end
 
-	def is_group_admin?
-		self.role.name == Role::GROUP_ADMIN
-	end
+  def is_group_admin?
+    self.role.name == Role::GROUP_ADMIN
+  end
 end
