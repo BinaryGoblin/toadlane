60346d705ea0dc4de77e56670986193145bcc159
Diganta Mandal <diganta@codeapogee.com>
2017-06-02 00:00:40 +0530

    f20170601362-synapsepay_level_2_verification_process
    * Updated process of updating synapsepay documents.

--- app/toadlane/services/fly_and_buy/company_document.rb
+++ app/toadlane/services/fly_and_buy/company_document.rb
@@ -16,7 +16,7 @@ module Services
         synapse_user = synapse_pay.user(user_id: fly_buy_profile.synapse_user_id)
         create_or_update_base_document(synapse_user)
 
-        update_fly_buy_profile(synapse_company_doc_id: synapse_company_doc_id) unless fly_buy_profile.synapse_company_doc_id.present?
+        update_fly_buy_profile(synapse_company_doc_id: synapse_company_doc_id)
       rescue SynapsePayRest::Error => e
         update_fly_buy_profile(error_details: e.response['error'])
       end
@@ -24,12 +24,10 @@ module Services
       private
 
       def create_or_update_base_document(synapse_user)
-        if fly_buy_profile.synapse_company_doc_id.present?
-          base_document = SynapsePayRest::BaseDocument.new(user: synapse_user, id: fly_buy_profile.synapse_company_doc_id)
-          base_document.update(payload)
-        else
-          synapse_user.create_base_document(payload)
-        end
+        remove_base_document(synapse_user) if fly_buy_profile.synapse_company_doc_id.present?
+
+        synapse_user = reload_synapse_user
+        synapse_user.create_base_document(payload)
       end
 
       def payload
@@ -38,7 +36,7 @@ module Services
           phone_number: user.phone,
           ip: fly_buy_profile.synapse_ip_address,
           name: company_name,
-          aka: user.company,
+          aka: company_name,
           entity_type: fly_buy_profile.entity_type,
           entity_scope: fly_buy_profile.entity_scope,
           birth_day: fly_buy_profile.date_of_company.day,
@@ -69,13 +67,23 @@ module Services
       end
 
       def company_name
-        [user.first_name, user.company].join(' ')
+        user.company
+      end
+
+      def remove_base_document(synapse_user)
+        base_document = SynapsePayRest::BaseDocument.new(user: synapse_user, id: fly_buy_profile.synapse_company_doc_id)
+        base_document.update(permission_scope: 'DELETE_DOCUMENT')
+      rescue SynapsePayRest::Error::NotFound
       end
 
       def synapse_company_doc_id
-        synapse_user = synapse_pay.user(user_id: fly_buy_profile.synapse_user_id)
+        synapse_user = reload_synapse_user
         base_document = synapse_user.base_documents.find { |doc| doc.name == company_name }
         base_document.present? ? base_document.id : nil
+      end
+
+      def reload_synapse_user
+        synapse_pay.user(user_id: fly_buy_profile.synapse_user_id)
       end
     end
   end
