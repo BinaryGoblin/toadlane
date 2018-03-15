8060d7b0e3775d0fef9ce6ba525a532d66fcca07
Isaac Laris <isaac_laris@yahoo.com>
2018-03-14 21:59:40 -0700

    add paperclip image file attributes to model/request.rb

--- app/models/request.rb
+++ app/models/request.rb
@@ -15,4 +15,15 @@
 class Request < ActiveRecord::Base
   belongs_to :sender, class_name: 'User', foreign_key: :sender_id
   belongs_to :receiver, class_name: 'User', foreign_key: :receiver_id
+
+  
+  validates_presence_of :image_file_name, :image_file_size, :image_content_type
+
+  has_attached_file :image, styles: {
+                                small: '110x95#',
+                                medium: '240x225#',
+                                big: '620x420#'
+                              },
+                              default_url: '/images/products/:style/missing.png'
+  do_not_validate_attachment_file_type :image
 end
